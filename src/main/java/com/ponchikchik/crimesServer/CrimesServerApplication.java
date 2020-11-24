8fdeeeddb0d3076a8d2e92831f8af95d69984ed9
Aleksandr Agafonov <alex.2exe2@gmail.com>
2020-11-24 11:29:03 +0500

    init commit

---
+++ src/main/java/com/ponchikchik/crimesServer/CrimesServerApplication.java
@@ -0,0 +1,13 @@
+package com.ponchikchik.crimesServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ponchikchik/crimesServer/CrimesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponchikchik.crimesServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
